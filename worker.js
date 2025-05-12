5d3611b678a51fe05eb5c310f640e23c73fafecc
longxiao7 <nyaa@qq.com>
2025-05-13 03:14:57 +0800

    up

--- worker.js
+++ worker.js
@@ -1,22 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-
 export default {
   async fetch(request, env, ctx) {
     //
-    if(request.url='https://list.th123.uno/data'){
+    if(/data$/.test(request.url)){
       let data = await fetch('https://konni.delthas.fr/games');
-      return new Response(data);
+      let result = new Response(data.body,
+        {
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        }
+        );
+      return result;
     }else{
-      return new Response('404');
+      return new Response('4042');
     }
   },
 };
